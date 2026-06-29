6ddc478069e1248508f9c125a74387d73b66307d
buraksaiyan <burak.koken@std.antalya.edu.tr>
2026-06-29 21:52:11 +0300

    feat: initialize Spring Boot project structure with Maven wrapper

---
+++ acente-yonetim/src/main/java/com/acente/acente_yonetim/AcenteYonetimApplication.java
@@ -0,0 +1,13 @@
+package com.acente.acente_yonetim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcenteYonetimApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcenteYonetimApplication.class, args);
+	}
+
+}
