eed08d6e4e14571275e90c1a785da246a630db75
buraksaiyan <burak.koken@std.antalya.edu.tr>
2026-06-30 12:36:37 +0300

    feat: initialize docker-compose for postgres and add guest and hotel entity classes

---
+++ acente-yonetim/src/main/java/com/acente/acente_yonetim/entity/guest.java
@@ -0,0 +1,14 @@
+package com.acente.acente_yonetim.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
+@Entity
+public class guest {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+}
