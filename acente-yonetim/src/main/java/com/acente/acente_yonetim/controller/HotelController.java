0526ae766bf1c56d91667cbf685533e13f8d98bf
buraksaiyan <burak.koken@std.antalya.edu.tr>
2026-07-01 12:29:38 +0300

    feat: implement CRUD endpoints for HotelController

--- acente-yonetim/src/main/java/com/acente/acente_yonetim/controller/HotelController.java
+++ acente-yonetim/src/main/java/com/acente/acente_yonetim/controller/HotelController.java
@@ -1,5 +1,50 @@
 package com.acente.acente_yonetim.controller;
 
-public class HotelController {
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
+import com.acente.acente_yonetim.entity.Hotel;
+import com.acente.acente_yonetim.service.HotelService;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+
+@RestController
+@RequestMapping("/hotels")
+
+public class HotelController {
+    private final HotelService hotelService;
+
+    public HotelController(HotelService hotelService) {
+        this.hotelService = hotelService;
+    }
+
+    @GetMapping
+    public List<Hotel> getAllHotels() {
+        return hotelService.getAllHotels();
+    }
+
+    @GetMapping("/{id}")
+    public Hotel getHotelById(@PathVariable Long id) {
+        return hotelService.getHotelById(id);
+    }
+
+    @PostMapping
+    public Hotel createHotel(@RequestBody Hotel hotel) {
+        return hotelService.createHotel(hotel);
+    }
+
+    @PutMapping("/{id}")
+    public Hotel updateHotel(@PathVariable Long id, @RequestBody Hotel hotelDetails) {
+        return hotelService.updateHotel(id, hotelDetails);
+    }
+
+    @DeleteMapping("/{id}")
+    public void deleteHotel(@PathVariable Long id) {
+        hotelService.deleteHotel(id);
+    }
 }
